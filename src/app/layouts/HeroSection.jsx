dbcf15e64ffbc57e6154624e958ea8b387efa58c
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-12-07 14:27:09 +0100

    hero text added

--- src/app/layouts/HeroSection.jsx
+++ src/app/layouts/HeroSection.jsx
@@ -4,6 +4,13 @@ export default function HeroSection() {
 	return (
 		<main>
 			<Navbar />
+			<section className='hero-content'>
+				<h1 className='title'>
+					Middle Earth Travel, the truely authentic New Zealand
+					Experience
+				</h1>
+				<p className='sub-title'>For all the Lord of the Rings nerds</p>
+			</section>
 		</main>
 	)
 }
