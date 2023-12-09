87838abbfe5a03d7bb8fd3417af5353535d64100
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-12-09 14:35:52 +0100

    .

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,31 @@ export default function Home() {
 				Pellentesque sit amet leo vitae libero consectetur condimentum.
 				Nulla laoreet metus at sapien mollis bibendum.
 			</div>
+			<div className='full-width'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Pellentesque sit amet leo vitae libero consectetur condimentum.
+				Nulla laoreet metus at sapien mollis bibendum.
+			</div>
+			<div className='right-side'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Pellentesque sit amet leo vitae libero consectetur condimentum.
+				Nulla laoreet metus at sapien mollis bibendum.
+			</div>
+			<div className='left-side'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Pellentesque sit amet leo vitae libero consectetur condimentum.
+				Nulla laoreet metus at sapien mollis bibendum.
+			</div>
+			<div className='far-right-side'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Pellentesque sit amet leo vitae libero consectetur condimentum.
+				Nulla laoreet metus at sapien mollis bibendum.
+			</div>
+			<div className='far-left-side'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Pellentesque sit amet leo vitae libero consectetur condimentum.
+				Nulla laoreet metus at sapien mollis bibendum.
+			</div>
 		</main>
 	)
 }
