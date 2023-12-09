844ceb3a3c7dd6802767d558958e48f9c47c646e
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-12-09 15:56:14 +0100

    grid added for main content

--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,50 @@
 export default function Home() {
 	return (
 		<main className='content-grid'>
-			<div className=''>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				Pellentesque sit amet leo vitae libero consectetur condimentum.
-				Nulla laoreet metus at sapien mollis bibendum.
-			</div>
-			<div className='breakout'>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				Pellentesque sit amet leo vitae libero consectetur condimentum.
-				Nulla laoreet metus at sapien mollis bibendum.
-			</div>
-			<div className='full-width'>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				Pellentesque sit amet leo vitae libero consectetur condimentum.
-				Nulla laoreet metus at sapien mollis bibendum.
-			</div>
-			<div className='right-side'>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				Pellentesque sit amet leo vitae libero consectetur condimentum.
-				Nulla laoreet metus at sapien mollis bibendum.
-			</div>
-			<div className='left-side'>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				Pellentesque sit amet leo vitae libero consectetur condimentum.
-				Nulla laoreet metus at sapien mollis bibendum.
-			</div>
-			<div className='far-right-side'>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				Pellentesque sit amet leo vitae libero consectetur condimentum.
-				Nulla laoreet metus at sapien mollis bibendum.
-			</div>
-			<div className='far-left-side'>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-				Pellentesque sit amet leo vitae libero consectetur condimentum.
-				Nulla laoreet metus at sapien mollis bibendum.
-			</div>
+			<header className='header-container header-row'>
+				<div>Title</div>
+				<nav className='nav-container'>
+					<button>one</button>
+					<button>two</button>
+					<button>three</button>
+					<button>four</button>
+					<button className='white-button'>five</button>
+				</nav>
+				<button className='login-button'>Login</button>
+			</header>
+			<section className='hero-text breakout hero-text-row'>
+				<h1>this is hero text that will sit in the breakout area</h1>
+				<p>
+					this is also hero text but smaller taht will also sit in the
+					breakout hopefully if it is long enough
+				</p>
+			</section>
+			<section className='central-nav-row'>
+				<div className='central-nav '>
+					<button>one</button>
+					<button>two</button>
+					<button>three</button>
+					<button className='white-button'>search</button>
+				</div>
+			</section>
+			<section className='stat-cards stat-card-row'>
+				<div className='single-card'>
+					<p>number</p>
+					<p>text under number</p>
+				</div>
+				<div className='single-card'>
+					<p>number</p>
+					<p>text under number</p>
+				</div>
+				<div className='single-card'>
+					<p>number</p>
+					<p>text under number</p>
+				</div>
+				<div className='single-card'>
+					<p>number</p>
+					<p>text under number</p>
+				</div>
+			</section>
 		</main>
 	)
 }
