55053806300a407c1ff9760ea5f1cf6a91677f6f
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-12-09 17:03:34 +0100

    styles coppied

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,21 @@ export default function Home() {
 		<main className='content-grid'>
 			<div className='background-container full-width full-height'></div>
 			<header className='header-container header-row breakout'>
-				<div>Title</div>
+				<div className='title'>Title</div>
 				<nav className='nav-container'>
 					<button>one</button>
 					<button>two</button>
 					<button>three</button>
 					<button>four</button>
-					<button className='white-button'>five</button>
+					<button>five</button>
 				</nav>
 				<button className='login-button'>Login</button>
 			</header>
 			<section className='hero-text breakout hero-text-row'>
-				<h1>this is hero text that will sit in the breakout area</h1>
-				<p>
+				<h1 className='title'>
+					this is hero text that will sit in the breakout area
+				</h1>
+				<p className='sub-title'>
 					this is also hero text but smaller taht will also sit in the
 					breakout hopefully if it is long enough
 				</p>
